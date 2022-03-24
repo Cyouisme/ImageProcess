67ddd98f1332f2f025b2eb93004dd356ea826e37
DEV_DN_08 <baochunga1@gmail.com>
2022-03-24 16:57:01 +0700

    Fix l to i character

--- output_text_detection.py
+++ output_text_detection.py
@@ -5,7 +5,6 @@ from PIL import Image
 from input_text_detection import read_dataset
 import time
 
-
 start_time = time.time()
 
 
@@ -22,21 +21,27 @@ def overlapping_image(size1, size2):
     return False, image1
 
 
+def split(word):
+    return list(word)
+
+
 imgDict = {}
 dataset = read_dataset("images/data")
 
 
 def image_to_text():
-    #Read image form file and convert to gray image
+    # Read image form file and convert to gray image
     for f in Path('images/imageInput').rglob('*.[jp][pn]*'):
         im = Image.open(f.as_posix()).convert('L')
-        #Convert image to numpy arr
+        # Convert image to numpy arr
         im = np.array(im)
         # im = cv2.GaussianBlur(im, (3,3), cv2.BORDER_DEFAULT)
         # im = cv2.blur(im, (3,3))
         thresh = cv2.threshold(im, 127, 255, cv2.THRESH_BINARY_INV)[1]
         dilated = cv2.dilate(thresh.copy(), np.ones((5, 1), np.uint8), iterations=1)
         contours, _ = cv2.findContours(dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # cv2.drawContours(im, contours, -1, (0, 0, 0), -1)
+        # cv2.imshow('im', im)
         dir_images = {}
         list_images = []
         for c in contours:
@@ -45,12 +50,14 @@ def image_to_text():
                 dir_images.setdefault(x, ((x, y, w, h), thresh[y:y + h, x:x + w]))
             # cv2.rectangle(im, (x, y), (x + w, y + h), (128, 128, 128), 2)
         list_img = sorted(dir_images.items(), reverse=False)[:-1]
+        # cv2.imshow('im', im)
+        # cv2.waitKey()
         i = 0
         while i <= len(list_img) - 2:
             check, img = overlapping_image(list_img[i][1], list_img[i + 1][1])
-            #Compare the end point of the previous word with the first point of the following word
+            # Compare the end point of the previous word with the first point of the following word
             nx, ny, nw, nh = list_img[i + 1][1][0]
-            #Add image to list with image fixed and check 0-1 for space
+            # Add image to list with image fixed and check 0 - no_space and 1 - is_space
             list_images.append(
                 [cv2.copyMakeBorder(cv2.resize(img, (24, 43)), 30, 30, 30, 30, cv2.BORDER_CONSTANT, value=(0, 0, 0)),
                  1 if (nx - (list_img[i][1][0][0] + list_img[i][1][0][2])) > 7 else 0])
@@ -63,26 +70,37 @@ def image_to_text():
                      1 if (nx - (list_img[i][1][0][0] + list_img[i][1][0][2])) > 7 else 0])
             i += 1
         # Compare input and output image and print it to screen
+        word_list = []
         result = ''
+        character = []
+        dicnationaryVN = []
         for i, (image, isSpace) in enumerate(list_images):
             min_diff = []
             for image_data in dataset:
-                #Compare two images with number pixel different
-                #Stack two image and count white point -> different point
-                #The more similar two images are, the less white points they have
-                res = cv2.absdiff(image, image_data[1])
+                """ Compare two images with number pixel different
+                Stack two image and count white point -> different point
+                The more similar two images are, the less white points they have """
+                res = cv2.absdiff(image, image_data[1])  # absdiff() func to stack two images
                 diff_count = cv2.countNonZero(res)
                 if len(min_diff) == 0:
                     min_diff = [image_data[0], diff_count]
                 else:
                     if min_diff[1] > diff_count:
                         min_diff = [image_data[0], diff_count]
-            #Check space in sentence and print sentences from image to screen
+            # Check space in sentence and print sentences from image to screen
             if isSpace == 1:
                 result += min_diff[0] + ' '
             else:
                 result += min_diff[0]
-        print(result)
+        word_list = result.split(' ')
+        # Find word not begin with 'l' and replace 'i' to 'l' for them
+        for i, word in enumerate(word_list):
+            if 'l' in word:
+                if not word.startswith('l'):
+                    word_list[i] = word.replace('l', 'i')
+        # Print the result to screen
+        print(" ".join(word_list))
+
 
 
 image_to_text()
